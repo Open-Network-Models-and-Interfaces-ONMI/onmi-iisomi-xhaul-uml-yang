18df8ce5411e29a2ab19cc7b62077606f607f742
demx8as6 <martin@skorupski.de>
2016-03-09 22:29:16 +0100

    Example of a config ui

---
+++ 02-MWTN-PoC/code/ux/concept-config-ui/modules/concept/concept.service.js
@@ -0,0 +1,10 @@
+concept.factory('concept', function() {
+    'use strict';
+
+    
+    var service = {};
+
+    service.name = 'UX Concept';
+    
+    return service;
+});
---
+++ 02-MWTN-PoC/code/ux/concept-config-ui/modules/directives/footer/footer.js
@@ -0,0 +1,11 @@
+app.directive('htFooter', function() {
+    return {
+        restrict : 'E',
+        // replace: true, // important! do not un-comment
+        templateUrl : '/ux/modules/directives/footer/footer.html',
+        scope : {
+        },
+        controller : function() {
+        }
+    };
+});
