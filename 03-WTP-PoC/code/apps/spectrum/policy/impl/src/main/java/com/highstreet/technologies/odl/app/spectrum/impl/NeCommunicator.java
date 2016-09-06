046ccb5ff853ad09c79c36ba86ba93100762f45f
liuwei10121897 <liu.wei91@zte.com.cn>
2016-09-06 09:57:54 +0800

    refactoring : exclude some actions

---
+++ 03-WTP-PoC/code/apps/spectrum/policy/impl/src/main/java/com/highstreet/technologies/odl/app/spectrum/impl/NeCommunicator.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright © 2015 ZTE and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package com.highstreet.technologies.odl.app.spectrum.impl;
+
+/**
+ * Created by olinchy on 16-9-5.
+ */
+public interface NeCommunicator
+{
+    void set(Attribute attribute);
+
+    Attribute running(DN dn);
+}
--- 03-WTP-PoC/code/apps/spectrum/policy/impl/src/main/java/com/highstreet/technologies/odl/app/spectrum/impl/NotEqualConfigurationPolicy.java
+++ 03-WTP-PoC/code/apps/spectrum/policy/impl/src/main/java/com/highstreet/technologies/odl/app/spectrum/impl/NotEqualConfigurationPolicy.java
@@ -10,35 +10,21 @@ package com.highstreet.technologies.odl.app.spectrum.impl;
 /**
  * Created by olinchy on 16-9-3.
  */
-public class NotEqualConfigurationPolicy
+public abstract class NotEqualConfigurationPolicy
 {
+    private DataAgent agent;
+    private NeCommunicator neCommuicate;
+
     public void execute()
     {
         notEquals(
-                planned(new DN("some_airInterface")), running(new DN("some_airInterface")),
-                () -> set(planned(new DN("some_airInterface"))));
+                agent.planned(new DN("some_airInterface")), neCommuicate.running(new DN("some_airInterface")),
+                () -> neCommuicate.set(agent.planned(new DN("some_airInterface"))));
     }
 
     private void notEquals(Attribute planned, Attribute running, Then then)
     {
         if (planned.equals(running))
             then.then();
-    }
-
-    private void set(Attribute attr)
-    {
-        // set the attribute via netconf
-    }
-
-    private Attribute running(DN dn)
-    {
-        // fetch via netconf
-        return null;
-    }
-
-    private Attribute planned(DN dn)
-    {
-        // fetch via agent
-        return null;
     }
 }
