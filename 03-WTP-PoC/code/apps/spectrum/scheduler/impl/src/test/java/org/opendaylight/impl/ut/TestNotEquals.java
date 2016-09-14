fcd94b3a4a30402a2879cded2aa1d72667719670
liuwei10121897 <liu.wei91@zte.com.cn>
2016-09-14 17:55:56 +0800

    refactoring : ut code

---
+++ 03-WTP-PoC/code/apps/spectrum/scheduler/impl/src/test/java/org/opendaylight/impl/ut/TestNotEquals.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright © 2015 ZTE and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.impl.ut;
+
+import com.highstreet.technologies.odl.app.spectrum.impl.api.DataAgent;
+import com.highstreet.technologies.odl.app.spectrum.impl.api.NeCommunicator;
+import com.highstreet.technologies.odl.app.spectrum.impl.meta.Attribute;
+import com.highstreet.technologies.odl.app.spectrum.impl.meta.DN;
+import com.highstreet.technologies.odl.app.spectrum.impl.meta.Mo;
+import com.highstreet.technologies.odl.app.spectrum.impl.meta.Result;
+import com.highstreet.technologies.odl.app.spectrum.impl.policy.NotEqualConfigurationPolicy;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by olinchy on 16-9-14.
+ */
+public class TestNotEquals
+{
+    @Test
+    public void name() throws Exception
+    {
+        final boolean[] isSetCalled = {false};
+        NotEqualConfigurationPolicy.execute(new DataAgent()
+        {
+            @Override
+            public Object get(Attribute attr)
+            {
+                return "1";
+            }
+
+            @Override
+            public Result<Mo> find(
+                    String typeName)
+            {
+                return null;
+            }
+        }, new NeCommunicator()
+        {
+            @Override
+            public void set(Attribute attribute, Object value)
+            {
+                isSetCalled[0] = true;
+            }
+
+            @Override
+            public Object running(Attribute attr)
+            {
+                return "2";
+            }
+        }, new Attribute(new DN("/A/1"), "txFrequency"));
+
+        assertTrue(isSetCalled[0]);
+    }
+}
