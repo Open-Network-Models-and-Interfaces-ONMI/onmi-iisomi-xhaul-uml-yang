c23c9d7d75115b79fb3f5de2e6f6e27593ebf825
demx8as6 <martin@skorupski.de>
2016-10-21 22:24:50 +0200

    topology

--- 03-WTP-PoC/code/ux/mwtnTopology/mwtnTopology-module/src/main/resources/mwtnTopology/mwtnTopology.controller.js
+++ 03-WTP-PoC/code/ux/mwtnTopology/mwtnTopology-module/src/main/resources/mwtnTopology/mwtnTopology.controller.js
@@ -6,19 +6,66 @@
  * and is available at http://www.eclipse.org/legal/epl-v10.html
  */
 
-define(['app/mwtnTopology/mwtnTopology.module', 'app/mwtnTopology/mwtnTopology.services', 'app/mwtnCommons/mwtnCommons.services'], function(mwtnTopologyApp) {
+define(['app/mwtnTopology/mwtnTopology.module', 
+        'app/mwtnTopology/mwtnTopology.services', 
+        'app/mwtnCommons/mwtnCommons.services'], function(mwtnTopologyApp) {
 
-  mwtnTopologyApp.register.controller('mwtnTopologyCtrl', ['$scope', '$rootScope', '$mwtnTopology', '$mwtnCommons', '$mwtnLog', function($scope, $rootScope, $mwtnTopology, $mwtnCommons, $mwtnLog) {
+  mwtnTopologyApp.register.controller('mwtnTopologyCtrl', ['$scope', '$rootScope', '$mwtnTopology', '$mwtnLog', 
+                                             function($scope, $rootScope, $mwtnTopology, $mwtnLog) {
 
     $rootScope['section_logo'] = 'src/app/mwtnTopology/images/mwtnTopology.png'; // Add your topbar logo location here such as 'assets/images/logo_topology.gif'
+    $scope.oneAtATime = true;
+    $scope.layers = [
+        {
+          label : 'Site',
+          section : [
+              {
+                label : 'Map',
+                template : 'src/app/mwtnTopology/templates/maps.tpl.html'
+              },
+              {
+                label : 'Sites',
+                template : 'src/app/mwtnTopology/templates/grid.tpl.html'
+              },
+              {
+                label : 'Site links',
+                template : 'src/app/mwtnTopology/templates/grid.tpl.html'
+              } ]
+        },
+        {
+          label : 'Microwave / Miiiimeterwave',
+          section : [
+              {
 
-    $scope.mwtnTopologyInfo = {};
-
-    $mwtnCommons.getData(function(data){
-      $scope.data = data;      
-    });
-
+                label : 'Map',
+                template : 'src/app/mwtnTopology/templates/topology.tpl.html'
+              },
+              {
+                label : 'Air interfaces',
+                template : 'src/app/mwtnTopology/templates/grid.tpl.html'
+              },
+              {
+                label : 'MWR links',
+                template : 'src/app/mwtnTopology/templates/grid.tpl.html'
+              } ]
+        },
+        {
+          label : 'Ethernet',
+          section : [
+              {
+                label : 'Map',
+                template : 'src/app/mwtnTopology/templates/topology.tpl.html'
+              },
+              {
+                label : 'Ports (ETH-CTPs)',
+                template : 'src/app/mwtnTopology/templates/grid.tpl.html'
+              },
+              {
+                label : 'Ethernet connections',
+                template : 'src/app/mwtnTopology/templates/grid.tpl.html'
+              } ]
+        } ];
+    
   }]);
-
 
 });
