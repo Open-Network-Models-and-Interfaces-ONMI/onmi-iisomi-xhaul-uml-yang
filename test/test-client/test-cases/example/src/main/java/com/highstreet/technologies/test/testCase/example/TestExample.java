31132e95c1d39b85d960af0e23e5db4c3d854399
demx8as6 <martin@skorupski.de>
2016-12-25 21:05:00 +0100

    split address

--- test/test-client/test-cases/example/src/main/java/com/highstreet/technologies/test/testCase/example/TestExample.java
+++ test/test-client/test-cases/example/src/main/java/com/highstreet/technologies/test/testCase/example/TestExample.java
@@ -11,23 +11,24 @@ package com.highstreet.technologies.test.testCase.example;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.TimeZone;
 
-import com.highstreet.technologies.test.client.api.Address;
+import javax.ws.rs.core.MultivaluedHashMap;
+
 import com.highstreet.technologies.test.client.api.Attribute;
 import com.highstreet.technologies.test.client.api.Node;
 import com.highstreet.technologies.test.client.api.RestConfServer;
 import com.highstreet.technologies.test.client.api.Result;
 import com.highstreet.technologies.test.client.api.Value;
-import com.highstreet.technologies.test.client.builder.AddressBuilder;
-import com.highstreet.technologies.test.client.builder.AttributeBuilder;
-import com.highstreet.technologies.test.client.builder.NodeBuilder;
-import com.highstreet.technologies.test.client.builder.RestConfServerBuilder;
-import com.highstreet.technologies.test.client.builder.ValueBuilder;
-import com.highstreet.technologies.test.client.enums.ConditionalPackage;
+import com.highstreet.technologies.test.client.impl.NodeBuilder;
+import com.highstreet.technologies.test.client.impl.RestConfServerBuilder;
+import com.highstreet.technologies.test.client.impl.ValueBuilder;
+import com.highstreet.technologies.test.client.enums.AttributeNames;
+import com.highstreet.technologies.test.client.enums.Layer;
 import com.highstreet.technologies.test.client.enums.Protocol;
-import com.highstreet.technologies.test.client.enums.SubObjectClass;
-import com.highstreet.technologies.test.client.impl.TestClient;
+import com.highstreet.technologies.test.client.api.TestClient;
+import com.highstreet.technologies.test.client.impl.TestClientBuilder;
 
 /**
  * TestExample
@@ -67,28 +68,6 @@ public class TestExample {
 				.build();
 	}
 
-	private static Attribute createAttribute() {
-		
-		final ConditionalPackage conditionalPackage = ConditionalPackage.MW_AirInterface_Pac;
-		final String layerProtocol = "LP-MWPS-ifIndex2";
-		final SubObjectClass subObjectClass = SubObjectClass.airInterfaceConfiguration;
-		final String attributeName = "airInterfaceName";
-		
-		return new AttributeBuilder(conditionalPackage, layerProtocol, subObjectClass)
-				.setAttribute(attributeName)
-				.build();
-	}
-
-	private static Address createAddress() {
-		
-		final RestConfServer restConfServer = createRestConfServer();
-		final Node node = createNode();
-		final Attribute attribute = createAttribute();
-		
-		return new AddressBuilder(restConfServer, node, attribute)
-				.build();
-	}
-
 	private static String getTimeAsIso8601() {
 		TimeZone tz = TimeZone.getTimeZone("UTC");
 		DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
@@ -99,22 +78,29 @@ public class TestExample {
 	private static void runTestCaseAirInterfaceName() {
 
 		// create TestClient to get access to RestConf GET and SET methods
-		TestClient validationClient = new TestClient();
-		
-		// Define the entire address for a wireless attribute using restconf of ODL
-		Address address = createAddress();
+		final RestConfServer restConfServer = createRestConfServer();
+		final Node node = createNode();
+		final TestClient validationClient = new TestClientBuilder(restConfServer, node)
+				.build();
 
-		
+		// Define the entire address for a wireless attribute using restconf of ODL
+		final MultivaluedHashMap<Layer, String> layerProtocols = validationClient.getLayerProtocolIds();
+		final List<String> airInterfaces = layerProtocols.get(Layer.AIRINTERFACE);
+
+		final String layerProtocol = airInterfaces.get(airInterfaces.size() - 1);
+		final AttributeNames attributeName = AttributeNames.airInterfaceName_airInterfaceConfiguration;
+		final Attribute attribute = attributeName.getAttribute(layerProtocol);
+
 		// Start
 		String out = String.format("START Test for %1s",
-				address.getAttribute().getAttribute());
+				attribute.getAttribute());
 		System.out.println(out);
 
 		// read the interesting value
-		Result getResult = validationClient.get(address);
+		Result getResult = validationClient.get(attribute);
 		out = String.format("<<< [%1s] GET %2s: %3s (%4s)",
 				getTimeAsIso8601(),
-				address.getAttribute().getAttribute(),
+				attribute.getAttribute(),
 				getResult.getValue().getValue().toString(),
 				getResult.getValue().getValue().getClass().toString());
 		System.out.println(out);
@@ -122,10 +108,10 @@ public class TestExample {
 		// set the interesting value
 		String airInterfaceNameValue = "newAirInterfaceName";
 		Value<String> value = (Value<String>) new ValueBuilder(airInterfaceNameValue).build();
-		Result setResult = validationClient.set(address, value);
+		Result setResult = validationClient.set(attribute, value);
 		out = String.format(">>> [%1s] SET %2s to %3s (%4s); Status: %5s: %6s",
 				getTimeAsIso8601(),
-				address.getAttribute().getAttribute(),
+				attribute.getAttribute(),
 				airInterfaceNameValue,
 				value.getValue().getClass().toString(),
 				setResult.getStatus(),
@@ -133,10 +119,10 @@ public class TestExample {
 		System.out.println(out);
 		
 		// read the interesting value
-		Result result = validationClient.get(address);
+		Result result = validationClient.get(attribute);
 		out = String.format("<<< [%1s] GET %2s: %3s (%4s)",
 				getTimeAsIso8601(),
-				address.getAttribute().getAttribute(),
+				attribute.getAttribute(),
 				result.getValue().getValue().toString(),
 				result.getValue().getValue().getClass().toString());
 		System.out.println(out);
