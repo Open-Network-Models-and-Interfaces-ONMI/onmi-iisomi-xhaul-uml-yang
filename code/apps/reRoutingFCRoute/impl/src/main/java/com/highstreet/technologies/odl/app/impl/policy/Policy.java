6a503ffcb2e2c6fcbc70d6e8624cce9051d17fe4
olinchy <olinchy@icloud.com>
2017-05-14 16:49:01 +0800

    bugfix : add file header

--- code/apps/reRoutingFCRoute/impl/src/main/java/com/highstreet/technologies/odl/app/impl/policy/Policy.java
+++ code/apps/reRoutingFCRoute/impl/src/main/java/com/highstreet/technologies/odl/app/impl/policy/Policy.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright © 2016 highstreet technologies GmbH and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
 package com.highstreet.technologies.odl.app.impl.policy;
 
 import com.highstreet.technologies.odl.app.impl.topology.Vertex;
