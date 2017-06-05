d6b594574f28f38981959f4e12c188f950dd0e5d
olinchy <olinchy@icloud.com>
2017-06-05 23:42:36 +0800

    feature : all api

--- code/apps/route/impl/src/main/java/com/highstreet/technologies/odl/app/impl/delegates/FC.java
+++ code/apps/route/impl/src/main/java/com/highstreet/technologies/odl/app/impl/delegates/FC.java
@@ -7,6 +7,9 @@
  */
 package com.highstreet.technologies.odl.app.impl.delegates;
 
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.route.rev150105.fc_desc.Fc;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.route.rev150105.fc_desc.FcBuilder;
+
 /**
  * Created by odl on 17-6-5.
  */
@@ -15,4 +18,13 @@ public class FC
     public String node;
     public String aEnd;
     public String zEnd;
+
+    public Fc toFc()
+    {
+        FcBuilder fcBuilder = new FcBuilder();
+        fcBuilder.setNodeName(node);
+        fcBuilder.setAEnd(aEnd);
+        fcBuilder.setZEnd(zEnd);
+        return fcBuilder.build();
+    }
 }
