683478eab2d9a6ec8a5e610d41826e9b52a890ea
demx8as6 <martin@skorupski.de>
2017-06-06 10:01:29 +0200

    mwtnBrowser

---
+++ code/ux/mwtnBrowser/mwtnBrowser-module/src/main/resources/mwtnBrowser/mwtnBrowser.module.js
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) 2016 highstreet technologies GmbH and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+define(['angularAMD', 
+        'app/routingConfig', 
+        'app/core/core.services', 
+        'common/config/env.module',
+        'app/mwtnCommons/mwtnCommons.module',
+        'app/mwtnCommons/bower_components/angular-clipboard/angular-clipboard'], function(ng) {
+  var mwtnBrowserApp = angular.module('app.mwtnBrowser', ['ui.grid', 'ui.bootstrap', 'app.core', 'ui.router.state', 'config', 'angular-clipboard']);
+
+  mwtnBrowserApp.config(function($stateProvider, $compileProvider, $controllerProvider, $provide, NavHelperProvider, $translateProvider) {
+    mwtnBrowserApp.register = {
+      controller : $controllerProvider.register,
+      directive : $compileProvider.directive,
+      factory : $provide.factory,
+      service : $provide.service
+    };
+
+    NavHelperProvider.addControllerUrl('app/mwtnBrowser/mwtnBrowser.controller');
+    NavHelperProvider.addToMenu('mwtnBrowser', {
+     "link" : "#/mwtnBrowser",
+     "active" : "main.mwtnBrowser",
+     "title" : "MWTN Config",
+     "icon" : "fa fa-cogs",  // Add navigation icon css class here
+     "page" : {
+        "title" : "MWTN Config",
+        "description" : "mwtnBrowser"
+     }
+    });
+
+    var access = routingConfig.accessLevels;
+
+    $stateProvider.state('main.mwtnBrowser', {
+        url: 'mwtnBrowser',
+        access: access.admin,
+        views : {
+            content : {
+                templateUrl: 'src/app/mwtnBrowser/templates/frame.tpl.html',
+                controller: 'mwtnBrowserCtrl'
+            }
+        }
+    });
+
+  });
+
+  return mwtnBrowserApp;
+});
