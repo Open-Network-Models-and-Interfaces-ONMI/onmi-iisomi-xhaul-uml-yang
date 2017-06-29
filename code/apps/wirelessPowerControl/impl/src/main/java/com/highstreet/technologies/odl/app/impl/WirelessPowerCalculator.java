f205552da00d5efd6a006111b9530bbd7735d38f
lbeles <lbeles@frinx.io>
2017-06-29 12:27:55 +0200

    Change alg with more interfaces

--- code/apps/wirelessPowerControl/impl/src/main/java/com/highstreet/technologies/odl/app/impl/WirelessPowerCalculator.java
+++ code/apps/wirelessPowerControl/impl/src/main/java/com/highstreet/technologies/odl/app/impl/WirelessPowerCalculator.java
@@ -27,20 +27,18 @@ public class WirelessPowerCalculator {
     public static final double THROUGHPUT_MARGIN_RATE_HIGH = 8/10.0; /* protecting margin level high */
     public static final double THROUGHPUT_MARGIN_RATE_LOW = 5/10.0; /* protecting margin level low */
 
-    private final MwAirInterfacePac air;
+    private final List<MwAirInterfacePac> airList;
     private final MwEthernetContainerPac eth;
     private WirelessPowerControlImpl impl;
 
-    public WirelessPowerCalculator(MwAirInterfacePac airInterfacePac, MwEthernetContainerPac ethernetContainerPac, WirelessPowerControlImpl impl) {
-        this.air = airInterfacePac;
+    public WirelessPowerCalculator(List<MwAirInterfacePac> mwAirInterfacePacList, MwEthernetContainerPac ethernetContainerPac, WirelessPowerControlImpl impl) {
+        this.airList = mwAirInterfacePacList;
         this.eth = ethernetContainerPac;
         this.impl = impl;
     }
 
     public void calc() {
         LOG.info("WirelessPowerCalculator start");
-
-//        "period-15-min";
 
         List<HistoricalPerformanceDataList> historicalPerfomanceList = new ArrayList<>();
 
@@ -54,6 +52,8 @@ public class WirelessPowerCalculator {
         if (!firstTime && historicalPerfomanceList.size() > 1) {
             Long histPerfCapacity1 = historicalPerfomanceList.get(0).getPerformanceData().getTxEthernetBytesSum();
             Long histPerfCapacity2 = historicalPerfomanceList.get(1).getPerformanceData().getTxEthernetBytesSum();
+            LOG.info("Historical capacity 1: {} bytes",histPerfCapacity1);
+            LOG.info("Historical capacity 2: {} bytes",histPerfCapacity1);
             if (histPerfCapacity1.equals(histPerfCapacity2)) {
                 return;
             }
@@ -73,50 +73,57 @@ public class WirelessPowerCalculator {
             return;
         }
 
-
-        byte txPower = air.getAirInterfaceConfiguration().getTxPower();
-        LOG.info("Current txPower: {} ", txPower);
-
         Double currentPerfCapacity = currentPerfomanceList.get(0).getPerformanceData().getTxEthernetBytesSum() * 8 / 15 / 60 / 1000000.0;
-        Double currentCapacity = air.getAirInterfaceConfiguration().getTxChannelBandwidth() * log2(air.getAirInterfaceStatus().getModulationCur()) * air.getAirInterfaceStatus().getCodeRateCur() / 100  / 1.15 / 1000 ;
+        LOG.debug("Current performance capacity of ETC: {} bytes per 15 min",currentPerfomanceList.get(0).getPerformanceData().getTxEthernetBytesSum());
+        LOG.debug("Current performance capacity of ETC: {} Mbs",currentPerfCapacity);
 
-        if (currentPerfCapacity < currentCapacity * THROUGHPUT_MARGIN_RATE_HIGH) {
-            if (txPower > 0) {
-                txPower--;
-            }
-            mergeAirConfiguration(txPower);
-        } else {
-            if (txPower < 127) {
-                txPower++;
-            }
-            mergeAirConfiguration(txPower);
+
+        double mwCapacityTotal = 0.0d;
+        for (MwAirInterfacePac air : airList) {
+            Double mwCapacity = air.getAirInterfaceConfiguration().getTxChannelBandwidth() * log2(air.getAirInterfaceStatus().getModulationCur()) * air.getAirInterfaceStatus().getCodeRateCur() / 100  / 1.15 / 1000 ;
+            mwCapacityTotal = mwCapacityTotal + mwCapacity;
+            LOG.debug("Air Interface capacity {} is: {} Mbs",air.getAirInterfaceConfiguration().getAirInterfaceName(), mwCapacity);
         }
 
-        LOG.info("New txPower: {} ", txPower);
+        LOG.debug("Total MWCapacity: {} Mbs",mwCapacityTotal);
+        LOG.debug("Factor: {}",THROUGHPUT_MARGIN_RATE_HIGH);
+
+
+        if (currentPerfCapacity < mwCapacityTotal * THROUGHPUT_MARGIN_RATE_HIGH) {
+            int i = 0;
+            for (MwAirInterfacePac air : airList) {
+                byte txPower = air.getAirInterfaceConfiguration().getTxPower();
+                LOG.info("Current txPower {} is : {} ",air.getAirInterfaceConfiguration().getAirInterfaceName(),txPower);
+                if (txPower > 0) {
+                    txPower--;
+                }
+                LOG.info("New txPower {} is : {} ",air.getAirInterfaceConfiguration().getAirInterfaceName(),txPower);
+                mergeAirConfiguration(txPower, air, i);
+                i++;
+            }
+        } else {
+            int i = 0;
+            for (MwAirInterfacePac air : airList) {
+                byte txPower = air.getAirInterfaceConfiguration().getTxPower();
+                LOG.info("Current txPower {} is : {} ",air.getAirInterfaceConfiguration().getAirInterfaceName(),txPower);
+                if (txPower < 127) {
+                    txPower++;
+                }
+                LOG.info("New txPower {} is : {} ",air.getAirInterfaceConfiguration().getAirInterfaceName(),txPower);
+                mergeAirConfiguration(txPower, air, i);
+                i++;
+            }
+        }
+
+
         firstTime = false;
 
     }
 
-    private TransmissionModelContainer calculateTransmissionModel() {
-        TransmissionModelContainer transmissionModelContainer = new TransmissionModelContainer();
-        for (SupportedChannelPlanList supportedChannelPlanList :  air.getAirInterfaceCapability().getSupportedChannelPlanList()) {
-            if (supportedChannelPlanList.getTransmissionModeList() == null) {
-                continue;
-            }
-
-            for (TransmissionModeList transmissionModeList : supportedChannelPlanList.getTransmissionModeList()) {
-                transmissionModelContainer.add(transmissionModeList);
-            }
-        }
-
-        transmissionModelContainer.orderItems();
-        return transmissionModelContainer;
-    }
-
-    public void mergeAirConfiguration(byte txPower) {
+    public void mergeAirConfiguration(byte txPower, MwAirInterfacePac air, int index) {
         AirInterfaceConfigurationBuilder configurationBuilder = new AirInterfaceConfigurationBuilder(air.getAirInterfaceConfiguration());
         configurationBuilder.setTxPower(txPower);
-        impl.merge(configurationBuilder.build());
+        impl.merge(configurationBuilder.build(), index);
         wait1seconds();
     }
 
@@ -131,93 +138,4 @@ public class WirelessPowerCalculator {
     public static double log2( double a) {
         return Math.log(a) / Math.log(2);
     }
-}
-
-class TransmissionModelContainer {
-    private List<TransmissionModelItem> itemList;
-
-    public TransmissionModelItem findTransmissionMode(Short modulationCur, Byte codeRateCur) {
-        for (TransmissionModelItem item : itemList) {
-            if (item.getInnerItem().getModulationScheme().equals(modulationCur) && item.getInnerItem().getCodeRate().equals(codeRateCur)) {
-                return item;
-            }
-        }
-        if (itemList.size() > 0) { // if we don't find, we select the first one
-            return itemList.get(0);
-        }
-        return null;
-    }
-
-    public TransmissionModelItem findTheLowestTransmissionMode(long currentCapacity) {
-        for (TransmissionModelItem item : itemList) {
-            if (currentCapacity < item.getCapacity() * WirelessPowerCalculator.THROUGHPUT_MARGIN_RATE_HIGH) {
-                return item;
-            }
-        }
-        if (itemList.size() > 0) { // if we don't find, we select the first one
-            return itemList.get(0);
-        }
-        return null;
-    }
-
-    public TransmissionModelContainer() {
-        itemList = new ArrayList<>();
-    }
-
-    public void add(TransmissionModeList item) {
-        itemList.add(new TransmissionModelItem(item,calculateCapacity(item)));
-    }
-
-
-
-    private Integer calculateCapacity(TransmissionModeList item) {
-        return (int)(item.getChannelBandwidth()*WirelessPowerCalculator.log2(item.getModulationScheme()) * item.getCodeRate());
-    }
-
-    public void orderItems() {
-        itemList.sort((transmissionModelItem, t1) -> transmissionModelItem.getCapacity().compareTo(t1.getCapacity()));
-    }
-}
-
-class TransmissionModelItem   {
-
-    public TransmissionModelItem(TransmissionModeList innerItem, Integer capacity) {
-        this.innerItem = innerItem;
-        this.capacity = capacity;
-    }
-
-    private TransmissionModeList innerItem;
-    private Integer capacity;
-
-    public TransmissionModeList getInnerItem() {
-        return innerItem;
-    }
-
-    public Integer getCapacity() {
-        return capacity;
-    }
-}
-
-class Simulator {
-    private static final int COUNT_STEP = 3;
-    int currentStep;
-    int expectedRxLevel;
-    boolean isSimulateGreaterValue;
-
-    public Simulator(int expectedRxLevel, boolean isSimulateGreaterValue) {
-        this.expectedRxLevel = expectedRxLevel;
-        this.isSimulateGreaterValue = isSimulateGreaterValue;
-        this.currentStep = 0;
-    }
-
-    public int getRemoteAirRxLevel() {
-        currentStep++;
-        if (COUNT_STEP == currentStep) {
-            return isSimulateGreaterValue ? expectedRxLevel -1 :  expectedRxLevel + 1;
-        }
-        return isSimulateGreaterValue ? expectedRxLevel + 1 :  expectedRxLevel - 1;
-    }
-
-
-
 }
