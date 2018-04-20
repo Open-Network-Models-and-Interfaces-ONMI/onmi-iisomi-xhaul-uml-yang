f51b4e48cd4e08ab266f14a22d5b94bd80061f59
DmitryKhomchenko <dmitry.khomchenko@gmail.com>
2018-04-20 14:27:58 +0300

    Optical Path Manager app. 1st version. Created using Template app.

---
+++ code/apps/opticalpathmanager/impl/src/main/java/org/opendaylight/mwtn/opticalpathmanager/MountpointChangeListener.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright © 2018 sendate.eu and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.mwtn.opticalpathmanager;
+
+import org.opendaylight.yang.gen.v1.urn.opendaylight.netconf.node.topology.rev150114.NetconfNode;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.NodeId;
+
+public interface MountpointChangeListener {
+
+       /**
+     * Managed device is disconnected from node/mountpoint
+     * @param nNodeId id of the mountpoint
+     * @param nNode mountpoint contents
+     */
+    public void removeListenerOnNode(NodeId nNodeId, NetconfNode nNode);
+
+    /**
+     * Managed device is connected to node/mountpoint
+     * @param nNodeId id of the mountpoint
+     * @param nNode mountpoint contents
+     */
+    public void startListenerOnNode(NodeId nNodeId, NetconfNode nNode);
+
+    /**
+     * Creation indication of node/mountpoint
+     * @param nNodeId id of the mountpoint
+     * @param nNode mountpoint contents
+     */
+    public void mountpointNodeCreation(NodeId nNodeId, NetconfNode nNode);
+
+    /**
+     * Removal indication of node/mountpoint
+     * @param nNodeId id of the mountpoint
+     */
+    public void mountpointNodeRemoved(NodeId nNodeId);
+
+}
+
