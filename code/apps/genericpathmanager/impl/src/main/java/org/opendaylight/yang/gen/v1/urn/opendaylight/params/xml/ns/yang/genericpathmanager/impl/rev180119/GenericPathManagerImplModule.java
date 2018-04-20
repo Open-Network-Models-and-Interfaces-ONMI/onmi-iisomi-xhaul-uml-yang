d2bbc8a2db539a211b2efe0a5dbf92d8aa4237c2
hanifkukkalli <hanif.kukkalli@gmail.com>
2018-04-20 14:59:00 +0200

    Added yang generated files

---
+++ code/apps/genericpathmanager/impl/src/main/java/org/opendaylight/yang/gen/v1/urn/opendaylight/params/xml/ns/yang/genericpathmanager/impl/rev180119/GenericPathManagerImplModule.java
@@ -0,0 +1,22 @@
+package org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.genericpathmanager.impl.rev180119;
+public class GenericPathManagerImplModule extends org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.genericpathmanager.impl.rev180119.AbstractGenericPathManagerImplModule {
+    public GenericPathManagerImplModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver) {
+        super(identifier, dependencyResolver);
+    }
+
+    public GenericPathManagerImplModule(org.opendaylight.controller.config.api.ModuleIdentifier identifier, org.opendaylight.controller.config.api.DependencyResolver dependencyResolver, org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.genericpathmanager.impl.rev180119.GenericPathManagerImplModule oldModule, java.lang.AutoCloseable oldInstance) {
+        super(identifier, dependencyResolver, oldModule, oldInstance);
+    }
+
+    @Override
+    public void customValidation() {
+        // add custom validation form module attributes here.
+    }
+
+    @Override
+    public java.lang.AutoCloseable createInstance() {
+        // TODO:implement
+        throw new java.lang.UnsupportedOperationException();
+    }
+
+}
