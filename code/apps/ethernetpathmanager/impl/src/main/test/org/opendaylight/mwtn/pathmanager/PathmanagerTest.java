d7812389bb8b3e67e4c1e384a40fbff8a5215535
Herbert <herbert.eiselt@highstreet-technologies.com>
2018-05-28 18:26:17 +0200

    Last version of topology.json

--- code/apps/ethernetpathmanager/impl/src/main/test/org/opendaylight/mwtn/pathmanager/PathmanagerTest.java
+++ code/apps/ethernetpathmanager/impl/src/main/test/org/opendaylight/mwtn/pathmanager/PathmanagerTest.java
@@ -1,15 +1,11 @@
 package org.opendaylight.mwtn.pathmanager;
 
-
 public class PathmanagerTest {
 
 	public static void main(String[] args)
 	{
 
 		System.out.println("Pathmanager test program");
-
-
-
 
 	}
 
