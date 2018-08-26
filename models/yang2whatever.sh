ed2e7bd75c16e833b7a8955eb9e2f5e99e8cc0f4
demx8as6 <martin.skorupski@highstreet-technologies.com>
2018-08-26 15:37:42 +0200

    generate tree and yin format for mw 1.1

---
+++ models/yang2whatever.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+################################################################################
+# Script to convert yang modules into other formats
+# Author: martin.skorupski@highstreet-technologies.com
+# 
+# Copyright 2018 higshtreet technologies GmbH
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# 
+
+yangs="./yang";
+
+function filename {
+    x=${1%.*}
+    y=${x/$yangs/\.\/$2}
+    echo $y"."$2;
+}  
+
+function convert {
+    FILENAME=$(filename $1 $2);
+    pyang --path "$yangs:$yangs/ietf" --format $2 --output $FILENAME $1;
+}  
+
+for yang in $yangs/*.yang; do
+    echo "convert file: "$yang;
+    $(convert $yang "tree");
+    $(convert $yang "yin");
+    echo "done!";
+    echo;
+done
