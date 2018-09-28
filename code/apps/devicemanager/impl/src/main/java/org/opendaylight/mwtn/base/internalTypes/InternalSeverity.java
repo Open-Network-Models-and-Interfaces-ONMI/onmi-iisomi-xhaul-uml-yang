9fb9bdb04821637eddc887718a7db7d8029223a9
Herbert <herbert.eiselt@highstreet-technologies.com>
2018-09-28 16:34:24 +0200

    adapted yang specs for microwave and photonics

--- code/apps/devicemanager/impl/src/main/java/org/opendaylight/mwtn/base/internalTypes/InternalSeverity.java
+++ code/apps/devicemanager/impl/src/main/java/org/opendaylight/mwtn/base/internalTypes/InternalSeverity.java
@@ -45,23 +45,46 @@ public enum InternalSeverity {
 
     /**
      * convert ONF 1.2 Severity
-     * @param severity as input
+     * @param problemSeverity as input
      * @return String with related output
      */
-    public static InternalSeverity valueOf(org.opendaylight.yang.gen.v1.urn.onf.params.xml.ns.yang.microwave.model.rev170324.SeverityType severity ) {
-        switch( severity ) {
-            case NonAlarmed:
-                return InternalSeverity.NonAlarmed;
-            case Warning:
-                return InternalSeverity.Warning;
-            case Minor:
-                return InternalSeverity.Minor;
-            case Major:
-                return InternalSeverity.Major;
-            case Critical:
-                return InternalSeverity.Critical;
-        }
-        return null;
+    public static InternalSeverity valueOf(
+    		org.opendaylight.yang.gen.v1.urn.onf.params.xml.ns.yang.microwave.model.rev170324.SeverityType problemSeverity ) {
+    	switch( problemSeverity ) {
+	    	case NonAlarmed:
+	    		return InternalSeverity.NonAlarmed;
+	    	case Warning:
+	    		return InternalSeverity.Warning;
+	    	case Minor:
+	    		return InternalSeverity.Minor;
+	    	case Major:
+	    		return InternalSeverity.Major;
+	    	case Critical:
+	    		return InternalSeverity.Critical;
+    	}
+    	return null;
+    }
+
+    /**
+     *
+     * @param problemSeverity
+     * @return
+     */
+    public static InternalSeverity valueOf(
+    		org.opendaylight.yang.gen.v1.urn.onf.params.xml.ns.yang.microwave.model.rev180927.SeverityType problemSeverity) {
+    	switch( problemSeverity ) {
+	    	case NonAlarmed:
+	    		return InternalSeverity.NonAlarmed;
+	    	case Warning:
+	    		return InternalSeverity.Warning;
+	    	case Minor:
+	    		return InternalSeverity.Minor;
+	    	case Major:
+	    		return InternalSeverity.Major;
+	    	case Critical:
+	    		return InternalSeverity.Critical;
+    	}
+    	return null;
     }
 
 
