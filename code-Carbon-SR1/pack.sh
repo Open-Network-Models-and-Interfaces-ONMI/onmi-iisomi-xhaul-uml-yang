5e9228971d459a1ad96bfabd9f435d0c561ce6f4
Herbert <herbert.eiselt@highstreet-technologies.com>
2018-10-12 11:51:35 +0200

    Description to create complete tar

---
+++ code-Carbon-SR1/pack.sh
@@ -0,0 +1,14 @@
+# Pack a tar.gz container with ODL Apps and Sim
+# 0. odl build build
+# 1. odl prepare
+# 2. odl ib nostart
+# 3. Kopieren
+# 3.1 cp -r ~/odl/Nextcloud/build/poc/poc.../taraddon/* ../distribution....
+# 3.2 cp Binary odl.sh to dist.../bin
+# 3.3 ln -s bin/odl.sh odl
+# 4. cd ..
+# 5. mv distribut.... distribution...-poc5
+# 6. tar -xzf ~/odl/Next../delivery/poc/poc5/distributionXYZ-poc5.015.006.tar.gz distribution
+
+
+
