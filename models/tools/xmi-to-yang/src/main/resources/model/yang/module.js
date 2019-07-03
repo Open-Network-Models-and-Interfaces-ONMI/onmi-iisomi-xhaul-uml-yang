f607bbb95ed8e8d3c03040355980132df9878a23
demx8as6 <martin.skorupski@highstreet-technologies.com>
2019-07-03 10:02:16 +0200

    Modify config
    revision and prefix statements removed from config.json
    
    [uml-to-yang: 001]

--- models/tools/xmi-to-yang/src/main/resources/model/yang/module.js
+++ models/tools/xmi-to-yang/src/main/resources/model/yang/module.js
@@ -69,6 +69,17 @@ Module.prototype.writeNode = function (store,layer) {
     this.contact = this.contact.replace(/\r\n/g, '\r\n' + PRE + '\t\t');
     contact = PRE + "\tcontact \"" + this.contact + "\";\r\n";
     var revision_stmt = "";
+    
+    if (!this.revision) {
+        this.revision = [
+            {
+              "date": "1970-01-01",
+              "description": "Revision definition missing in papyrus uml.",
+              "reference": "ONF-TR-512, RFC 6020 and RFC 6087"
+            }
+          ]
+    }
+
     for(var indx=0; indx<this.revision.length; indx++){
     var revis;
     //var date=new Date();
